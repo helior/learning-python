1eb384a46320ca3de5aca2f69e6402b2a4931863
Helior Colorado <me@helior.info>
2022-12-10 21:51:53 -0800

    hi hi

--- msg-parsing/main.py
+++ msg-parsing/main.py
@@ -1,8 +1,5 @@
 import extract_msg
 
-# with open('./strangeDate.msg') as file:
-#   msg = extract_msg.Message(file)
-#   # print(msg)
 msg = extract_msg.openMsg('./strangeDate.msg')
 print(msg.body)
 print(msg.headerDict)
